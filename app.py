bb93ebebc20a0d51f6d0c955731a806f0af18b63
Kevin Orr <KevinOrr54@gmail.com>
2014-07-31 13:42:47 -0400

    Changed homepage title, simplified routing
    	modified:   app.py
    	modified:   templates/index.html
    	modified:   templates/maps/connector.html

--- app.py
+++ app.py
@@ -8,14 +8,11 @@ import flask.ext.compress
 import config
 from util import *
 
-if len(sys.argv) == 2 and sys.argv[1] == 'debug':
-    debug = True
-else:
-    debug = False
+debug = 'debug' in sys.argv[1:]
+remote_access = 'remote' in sys.argv[1:]
+heroku = 'DYNO' in os.environ
 
-production = 'DYNO' in os.environ
-
-if production and debug:
+if heroku and debug:
     e = RuntimeError('Cannot run in debug mode in production environment')
     raise e
 
@@ -24,10 +21,11 @@ app = Flask('__name__')
 flask.ext.compress.Compress(app)
 
 
+@app.route('/index.html')
 @app.route('/')
 def main():
     return make_html_resp(render_template('index.html',
-        production = production, ga_setup_url = c.ga_setup_url))
+        heroku = heroku, ga_setup_url = c.ga_setup_url))
 
 @app.route('/maps/connector')
 def connector_map():
@@ -35,18 +33,14 @@ def connector_map():
     return make_html_resp(render_template('maps/connector.html',
         jq_url = c.jq_url, d3_url = c.d3_url, d3_css_url = c.qtip_css_url,
         image_url = image_url, ga_setup_url = c.ga_setup_url,
-        debug = debug, production = production))
+        debug = debug, heroku = heroku))
 
 ##########################
 # REDIRECTS / REFERENCES #
 ##########################
 
-@app.route('/index.html')
-def _main_redirect_1():
-    return flask.redirect(url_for('main'))
-
 @app.route('/favicon.ico')
-def icon():
+def favicon():
     return app.send_static_file('icons/favicon.ico')
 
 def get_absolute_url(relative):
@@ -58,5 +52,8 @@ def gwebmaster_verification():
     return make_html_resp(app.send_static_file('google43467aa7281da596.html'))
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=debug)
+    if debug:
+        app.run('127.0.0.1', True)
+    else:
+        app.run('0.0.0.0', False)
     
